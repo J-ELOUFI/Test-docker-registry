a74581045e2b327d13d0edc0c0a1bc705185ec11
J-ELOUFI <66631874+J-ELOUFI@users.noreply.github.com>
2020-08-04 15:58:12 +0200

    Update main.js

--- main.js
+++ main.js
@@ -7,6 +7,6 @@ var server = http.createServer(function (request, response) {
   response.end("Hello NodeApp!! \n");
 });
 
-// listen on localhost:8000
-server.listen(8000);
+// listen on localhost:5000
+server.listen(5000);
 console.log("Server listening on port 5000 :  http://192.168.10.84:5000");
